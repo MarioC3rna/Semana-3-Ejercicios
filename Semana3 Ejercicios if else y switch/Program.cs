a7ab960cb1e4fc0ded535e97aa9c880d153c221f
MARIO FERNANDO CERNA NAJERA <mcernan@miumg.edu.gt>
2024-02-22 15:03:52 -0600

    Commit

--- Semana3 Ejercicios if else y switch/Program.cs
+++ Semana3 Ejercicios if else y switch/Program.cs
@@ -1,65 +1,26 @@
-﻿{
-    Console.WriteLine("---Calcular el Área de una Figura Geométrica---");
-    Console.WriteLine("");
+﻿
 
-    Console.WriteLine("Elija que figura quiere calcular");
-    Console.WriteLine("1. Triangulo");
-    Console.WriteLine("2. Cuadrado");
-    Console.WriteLine("3. Circulo");
+Console.WriteLine("---Validar la edad para el Club---");
+Console.WriteLine("");
 
-    try
+Console.WriteLine("Ingrese su edad");
+try
+{
+
+
+    int n1 = int.Parse(Console.ReadLine());
+    if (n1 >= 18)
     {
-        int opcionEle = int.Parse(Console.ReadLine());
-
-        if (opcionEle == 1)
-        {
-            CalcularTriangulo();
-        }
-        else if (opcionEle == 2)
-        {
-            CalcularCuadrado();
-        }
-        else if (opcionEle == 3)
-        {
-            CalcularCirculo();
-        }
-        else
-        {
-            Console.WriteLine("Opcion no valida");
-        }
+        Console.WriteLine("Puedes entrar al Club");
     }
-    catch (FormatException)
+
+    else
     {
-        Console.WriteLine("Error: Por favor, introduzca un numero valido.");
-    }
-    catch (OverflowException)
-    {
-        Console.WriteLine("Error: El numero introducido no esta en el rango");
+        Console.WriteLine("No puedes entrar al Club");
     }
 }
-
-static void CalcularTriangulo()
+catch (FormatException)
 {
-    Console.WriteLine("Ingrese la base");
-    double baseT = double.Parse(Console.ReadLine());
-    Console.WriteLine("Ingrese la altura");
-    double alturaT = double.Parse(Console.ReadLine());
-    double areaT = (baseT * alturaT) / 2;
-    Console.WriteLine("El area del Triangulo es " + areaT);
+    Console.WriteLine("Error: Por favor, ingrese un número válido para la edad.");
 }
 
-static void CalcularCuadrado()
-{
-    Console.WriteLine("Ingrese el lado del cuadrado");
-    double ladoC = double.Parse(Console.ReadLine());
-    double areaC = ladoC * ladoC;
-    Console.WriteLine("El área del cuadrado es: " + areaC);
-}
-
-static void CalcularCirculo()
-{
-    Console.WriteLine("Ingrese el radio del círculo:");
-    double radioC = double.Parse(Console.ReadLine());
-    double areaC = Math.PI * Math.Pow(radioC, 2);
-    Console.WriteLine("El área del círculo es: " + areaC);
-}
