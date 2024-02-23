01cb13a728a445e7c75aaa6e3ceb35c137d6fcea
MARIO FERNANDO CERNA NAJERA <mcernan@miumg.edu.gt>
2024-02-22 18:37:08 -0600

    commit

--- Semana3 Ejercicios if else y switch/Program.cs
+++ Semana3 Ejercicios if else y switch/Program.cs
@@ -1,48 +1,32 @@
 ﻿
-Console.WriteLine("---Mostrar el Día de la Semana a partir de un Número---");
+Console.WriteLine("---Calcular el Importe a Pagar por un Servicio---");
 Console.WriteLine("");
-Console.WriteLine("Ingresa un numero del 1 al 7");
-
+Console.WriteLine("Elige que servicio quiere");
+Console.WriteLine("1.Lavado de Auto");
+Console.WriteLine("2.Cambio de aceite");
+Console.WriteLine("3.Revision Mecanica");
 try
 {
     int n1 = int.Parse(Console.ReadLine());
-    string Dia;
+    string Ser;
     switch (n1)
     {
         case 1:
-            Dia = "Lunes";
-            Console.WriteLine(Dia);
+
+            Ser = "Lavado de Auto ";
+            Console.WriteLine("El servicio que elijo es " + Ser + " el importe a pagar es de 100");
             break;
 
         case 2:
-            Dia = "Martes";
-            Console.WriteLine(Dia);
+            Ser = "Cambio de Aceite ";
+            Console.WriteLine("El servicio que elijo es " + Ser + " el importe a pagar es de 200");
             break;
 
         case 3:
-            Dia = "Miercoles";
-            Console.WriteLine(Dia);
+            Ser = "Revision Mecanica ";
+            Console.WriteLine("El servicio que elijo es " + Ser + " el importe a pagar es de 300");
             break;
 
-        case 4:
-            Dia = "Jueves";
-            Console.WriteLine(Dia);
-            break;
-
-        case 5:
-            Dia = "Viernes";
-            Console.WriteLine(Dia);
-            break;
-
-        case 6:
-            Dia = "Sabado";
-            Console.WriteLine(Dia);
-            break;
-
-        case 7:
-            Dia = "Domingo";
-            Console.WriteLine(Dia);
-            break;
 
         default:
             throw new Exception("Ese numero no esta en el rango ");
