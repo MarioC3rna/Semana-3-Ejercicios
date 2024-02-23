c997b41f0498030ada553586c4dd2f7868be103e
MARIO FERNANDO CERNA NAJERA <mcernan@miumg.edu.gt>
2024-02-22 18:36:21 -0600

    commit

--- Semana3 Ejercicios if else y switch/Program.cs
+++ Semana3 Ejercicios if else y switch/Program.cs
@@ -1,35 +1,51 @@
 ﻿
-Console.WriteLine("---Mostrar un Mensaje de Bienvenida en Diferentes Idiomas---");
+Console.WriteLine("---Mostrar el Día de la Semana a partir de un Número---");
 Console.WriteLine("");
-Console.WriteLine("Eliga el idioma que desee");
-Console.WriteLine("1.Español");
-Console.WriteLine("2.Ingles");
-Console.WriteLine("3.Frances");
+Console.WriteLine("Ingresa un numero del 1 al 7");
+
 try
 {
     int n1 = int.Parse(Console.ReadLine());
-    string Idioma;
+    string Dia;
     switch (n1)
     {
         case 1:
-
-            Idioma = "Español";
-            Console.WriteLine("--Bienvenido--");
+            Dia = "Lunes";
+            Console.WriteLine(Dia);
             break;
 
         case 2:
-            Idioma = "Ingles";
-            Console.WriteLine("--Welcome--");
+            Dia = "Martes";
+            Console.WriteLine(Dia);
             break;
 
         case 3:
-            Idioma = "Frances";
-            Console.WriteLine("--Accueillir-");
+            Dia = "Miercoles";
+            Console.WriteLine(Dia);
             break;
 
+        case 4:
+            Dia = "Jueves";
+            Console.WriteLine(Dia);
+            break;
+
+        case 5:
+            Dia = "Viernes";
+            Console.WriteLine(Dia);
+            break;
+
+        case 6:
+            Dia = "Sabado";
+            Console.WriteLine(Dia);
+            break;
+
+        case 7:
+            Dia = "Domingo";
+            Console.WriteLine(Dia);
+            break;
 
         default:
-            throw new Exception("Ese numero no esta en el rango");
+            throw new Exception("Ese numero no esta en el rango ");
 
     }
 }
